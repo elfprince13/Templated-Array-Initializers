aaa2f93d9c0cef77d72fbdf5382ad51328775d8b
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2015-04-14 10:20:12 -0400

    Initial Commit

---
+++ XArrayData/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XArrayData
+//
+//  Created by Thomas Dickerson on 4/14/15.
+//  Copyright (c) 2015 StickFigure Graphic Productions. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
